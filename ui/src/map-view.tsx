003940b2b868288d2cd498b3a8ce93bbb8938e1f
Rifat Döver <rifatdover@gmail.com>
2021-05-26 19:07:57 +0100

    - Added pin clustering.

--- ui/src/map-view.tsx
+++ ui/src/map-view.tsx
@@ -3,28 +3,64 @@ import {useEffect, useState} from "react"
 import {Feature, Map, MapBrowserEvent, View} from "ol";
 import {fromLonLat} from "ol/proj";
 import TileLayer from "ol/layer/Tile";
-import {OSM, Vector} from "ol/source";
+import {Cluster, OSM, Vector} from "ol/source";
 
 import "ol/ol.css";
 import {FeatureLike} from "ol/Feature";
 import VectorLayer from "ol/layer/Vector";
 import {GeoJSON} from "ol/format";
-import {Icon, Style} from "ol/style";
+import {Fill, Icon, Stroke, Style, Text} from "ol/style";
+import CircleStyle from "ol/style/Circle";
 
 const geoJson = new GeoJSON()
+const styleCache: any = {};
 
-const mapPinStyle = new Style({
-    image: new Icon({
-        src: "/img/map-pin-blue.png",
-        scale: 25 / 50,
-        anchor: [0.5, 1.0]
-    })
-});
+const mapPinStyle = (feature: Feature) => {
+
+    const size = feature.get('features').length;
+
+    let style = styleCache[size];
+    if (style) return style;
+
+    if (feature.get("features").length === 1)
+        return new Style({
+            image: new Icon({
+                src: "/img/map-pin-blue.png",
+                scale: 25 / 50,
+                anchor: [0.5, 1.0]
+            })
+        });
+    else {
+        const text = new Text({
+            text: size.toString(),
+            fill: new Fill({
+                color: '#fff',
+            }),
+        });
+        style = new Style({
+            image: new CircleStyle({
+                radius: 10,
+                stroke: new Stroke({
+                    color: '#fff',
+                }),
+                fill: new Fill({
+                    color: '#3399CC',
+                }),
+            }),
+            text: text
+        });
+    }
+
+    styleCache[size] = style;
+    return style;
+};
+
 
 export const MapView: React.FC = () => {
     const [map, setMap] = useState<Map | undefined>(undefined)
     const [featureLayer, setFeatureLayer] = useState<VectorLayer | undefined>()
     const [features, setFeatures] = useState<FeatureLike[]>([])
+    const [distance, setDistance] = useState<number>()
 
     useEffect(() => {
         const map = new Map({
@@ -51,7 +87,7 @@ export const MapView: React.FC = () => {
         if (map) {
             setFeatureLayer(addFeatureLayer(featureLayer, features))
         }
-    }, [map, features])
+    }, [map, features, distance])
 
     const loadFeatureData = () => {
         fetch("/api/geo-json")
@@ -77,7 +113,13 @@ export const MapView: React.FC = () => {
             features: features as Feature<any>[]
         });
 
-        newLayer.setSource(source);
+        const clusterSource = new Cluster({
+            distance,
+            source: source,
+        });
+
+
+        newLayer.setSource(clusterSource);
 
         return newLayer
     }
@@ -94,8 +136,16 @@ export const MapView: React.FC = () => {
             .catch(e => alert(`Error adding point: ${e.message}`))
             .then(loadFeatureData)
     }
-
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const parse = parseInt(event.target.value, 10)
+        if (parse > 0) setDistance(parse);
+    }
+    useEffect(console.log, [distance])
     return <div>
         <div id="map" style={{height: "500px", width: "500px"}}/>
+        <form>
+            <label htmlFor="distance">cluster distance</label>
+            <input value={String(distance)} onChange={handleChange} type="range" min="0" max="100" step="1"/>
+        </form>
     </div>
 }
